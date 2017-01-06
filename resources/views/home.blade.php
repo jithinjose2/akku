5a52c73b254191f449f61e254b3d4773b7113e0b
Suresh Velusamy <suresh.v@compassitesinc.com>
2017-01-06 20:58:10 +0530

    added option to show accessable users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,39 @@
 
 @section('content')
     <div class="container">
-        @if(Auth::user()->modules->isEmpty())
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                @if(Auth::user()->modules->isEmpty())
                     <div class="panel panel-warning">
-
                         <div class="panel-heading">Device configuration missing</div>
-
                         <div class="panel-body">
                             <p>Please configure your device</p>
                             <p class="text-center">
                                 <a class="btn btn-success" href="/configure"> setup your first
                                     device</a>
                             </p>
-
                         </div>
                     </div>
-                </div>
+                @else
+
+                    @foreach(Auth::user()->modules as $module)
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">Module : {{$module->name}}</div>
+                            <div class="panel-body">
+                                <p>{{$module->name}}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
+                @foreach(Auth::user()->assignedModules as $module)
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Module : {{$module->name}}</div>
+                        <div class="panel-body">
+                            <p>{{$module->name}}</p>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endif
+        </div>
     </div>
 @endsection
